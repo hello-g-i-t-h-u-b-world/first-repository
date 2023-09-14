79cee90e0a8fc583e778f69de831639c7d7ccc79
JAE SEONG LEE <zippy111188@gmail.com>
2023-09-14 11:13:20 +0900

    랜덤 난수 출력

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,12 @@
 def print_hello():
     print("Hello World!!")
+    print("안녕 세계")
+    
+import random
+
+def print_random():
+    print(random.randint(1, 1000))
 
 if __name__ == '__main__':
     print_hello()
-    
+    print_random()    
