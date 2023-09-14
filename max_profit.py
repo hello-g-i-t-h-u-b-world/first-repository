f263e568c656b42776173dbefd365dc9e13ff07e
JAE SEONG LEE <zippy111188@gmail.com>
2023-09-14 11:34:22 +0900

    commit

--- max_profit.py
+++ max_profit.py
@@ -4,6 +4,7 @@ def maxProfit_bruteforce (prices):
    for i, price in enumerate(prices):
        for j in range(i, len(prices)):
            max_price = max(prices[j] - price, max_price)
+           print(max_price)
 
    return
 
